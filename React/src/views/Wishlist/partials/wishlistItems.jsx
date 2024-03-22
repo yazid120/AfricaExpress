795c3d5c053e34166108f2d2710de11d2db44af8
Kevin_makalister <74493602+yazid120@users.noreply.github.com>
2024-03-22 01:43:10 +0000

    feat: wishlist items section

--- React/src/views/Wishlist/partials/wishlistItems.jsx
+++ React/src/views/Wishlist/partials/wishlistItems.jsx
@@ -36,36 +36,38 @@ const WishlistItem = ()=>{
 
   return(
     <>
-    {Array.isArray(WishItem) && WishItem.map((item) => (
-      <div className="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded" key={item.id}>
-        <div className="w-28">
-          <img src={image_path_uri+item.image} alt="product 6" className="w-full"/>
-        </div>
-        <div className="w-1/3">
-          <h2 className="text-gray-800 text-xl font-medium uppercase">
-            {item.name}
-          </h2>
-          <p className="text-gray-500 text-sm">
-            Availability: <span className="text-green-600">In Stock</span>
-          </p>
-        </div>
-        <div className="text-primary text-lg font-semibold">{item.price_unit} $</div>
-        <a href="#" className="px-6 py-2 text-center text-sm text-white bg-primary border border-primary
-           rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium">
-          add to cart
-        </a>
-
-        {/* delete item from wishlist button */}
-       <div className="text-2xl text-red-500 cursor-pointer hover:bg-grey" onClick={() => deleteItemWishlist(item.id)}>
-        <BsTrash/>
-       </div>
-
-        <div className="text-gray-600 cursor-pointer hover:text-primary">
-          <i className="fa-solid fa-trash" />
-        </div>
+    {Array.isArray(WishItem) && WishItem.length > 0 ? (
+    WishItem.map((item) => (
+    <div className="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded" key={item.id}>
+      <div className="w-28">
+        <img src={image_path_uri + item.image} alt="product 6" className="w-full" />
       </div>
-      ))
-      }
+      <div className="w-1/3">
+        <h2 className="text-gray-800 text-xl font-medium uppercase">
+          {item.name}
+        </h2>
+        <p className="text-gray-500 text-sm">
+          Availability: <span className="text-green-600">In Stock</span>
+        </p>
+      </div>
+      <div className="text-primary text-lg font-semibold">{item.price_unit} $</div>
+      <a href="#" className="px-6 py-2 text-center text-sm text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium">
+        add to cart
+      </a>
+
+      {/* delete item from wishlist button */}
+      <div className="text-2xl text-red-500 cursor-pointer hover:bg-grey" onClick={() => deleteItemWishlist(item.id)}>
+        <BsTrash />
+      </div>
+
+      <div className="text-gray-600 cursor-pointer hover:text-primary">
+        <i className="fa-solid fa-trash" />
+      </div>
+    </div>
+  ))
+  ) : (
+  <div className="text-gray-800 rounded p-4 bg-gray-100">No item added to wishlist</div>
+  )}
     </>
   )
 }
