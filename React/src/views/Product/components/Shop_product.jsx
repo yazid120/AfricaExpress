a5e1307766f920ac29dfcc9f1796b12cd754c182
Kevin_makalister <74493602+yazid120@users.noreply.github.com>
2024-05-08 00:51:08 +0100

    feat: search product setup

--- React/src/views/Product/components/Shop_product.jsx
+++ React/src/views/Product/components/Shop_product.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import {useNavigate} from "react-router-dom";
 
-function ShopProduct({products,SearchProdFilter}){
+function ShopProduct({products}){
   const [productelement, Setproductelement] = useState([]);
   const image_path_uri = '../src/assets/images/Products/';
 
@@ -14,16 +14,7 @@ function ShopProduct({products,SearchProdFilter}){
     <>
     <div className="">
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 m-4">
-    {products
-      .filter((val) => {
-        if (SearchProdFilter === '') {
-          return val;
-        } else if (
-          val.name.toLowerCase().includes(SearchProdFilter.toLowerCase())
-        ) {
-          return val;
-        }
-      }).map(product=>(
+    {products.map(product=>(
      <li key={product.id} className="product_el_wrapp bg-white w-full rounded-lg p-4 shadow-md cursor-pointer"
      onClick={()=>HandleProductArticle(product.id)}>
       <img src={image_path_uri+product.image} alt={product.name} className="h-auto p-8 rounded-t-lg h-48" />
