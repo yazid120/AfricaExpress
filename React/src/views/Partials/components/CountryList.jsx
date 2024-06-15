2f6838f38a9df9e327ed1ea0975ccff5c467a30b
Kevin_makalister <74493602+yazid120@users.noreply.github.com>
2024-06-16 00:48:09 +0100

    feat: fix select element country list

--- React/src/views/Partials/components/CountryList.jsx
+++ React/src/views/Partials/components/CountryList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import CustomSelect from './CustomSelect';
 import axios from 'axios';
 
 function CountryList() {
@@ -16,17 +17,10 @@ function CountryList() {
 
   return (
     <>
-    <select className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary">
-    {countries.map(country => (
-    <option key={country.name.common} value={country.cca2} className="flex items-center">
-      <p
-        className="w-5 h-5 mr-2"
-        style={{ backgroundImage: `url(${country.flags.png})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-      />
-      <p>{country.name.common}</p>
-    </option>
-  ))}
-  </select>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Country Selector</h1>
+      <CustomSelect countries={countries} />
+    </div>
   </>
   );
 }
